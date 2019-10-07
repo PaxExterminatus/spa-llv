8950fec0eae3f2d24f8fce7a83554bade20143ce
PaxExterminatus <paxexterminatus@gmail.com>
2019-10-08 02:48:50 +0300

    added stores

--- src/app/app.svelte
+++ src/app/app.svelte
@@ -5,20 +5,17 @@
 	import Catalog from 'lib/catalog/catalog.svelte'
 	import {CatalogEntity} from 'app/entity'
 
-	let catalog = {
-		count: undefined,
-		courses: undefined,
-	};
+	let catalog;
 
-	onMount(async () => {
-		await api.catalog.then(resp => {
+	onMount(() => {
+		api.catalog.then(resp => {
 			catalog = new CatalogEntity(resp.data);
 		});
 	});
 </script>
 
 <div class="spa-list-app">
-	{#if catalog.courses}
+	{#if catalog}
 	<Catalog products={catalog.courses} />
 	{/if}
 </div>
